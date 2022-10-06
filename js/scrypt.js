4dc9d0bff509ebda28ef2d947ecf8972078f1771
AndreaGallini <andrea.gallini01@libero.it>
2022-10-06 15:57:13 +0200

    all

---
+++ js/scrypt.js
@@ -0,0 +1,38 @@
+let nomeUtente = prompt('Come ti chiami?')
+let numeroKm = parseInt(prompt('Quanti chilometri vuoi fare?'));
+console.log(numeroKm);
+let etaUtente = parseInt(prompt('Quanti anni hai?'));
+console.log(etaUtente);
+document.getElementById('Km').innerHTML = numeroKm
+document.getElementById('nome').innerHTML = nomeUtente
+document.getElementById('eta').innerHTML = etaUtente 
+const costoalKm = 0.21;
+const minorenni = 18;
+const over65 = 65;
+let costoKm = numeroKm * costoalKm;
+let costo = Math.ceil(costoKm)
+let costoIntero = costo.toFixed()
+console.log(costoIntero)
+if(isNaN(numeroKm)|| isNaN(etaUtente)){
+    alert('ricarica la pagina')
+}
+if(etaUtente < minorenni){
+    let scontoMinorenni = (costoIntero * 20) / 100;
+    let costoMinorenni = costoIntero - scontoMinorenni;
+    console.log(costoMinorenni)
+    document.getElementById('prezzoriservato').innerHTML = costoMinorenni + ' ' + '€'
+
+    
+} else if( etaUtente > over65){
+    let scontoOver65 = (costoIntero * 40) / 100;
+    let costoOver65 = costoIntero - scontoOver65;
+    console.log(costoOver65)
+    document.getElementById('prezzoriservato').innerHTML = costoOver65 + ' ' + '€'
+
+}else if( (etaUtente > minorenni) || (etaUtente < over65)){
+    document.getElementById('prezzoriservato').innerHTML = costoIntero
+}
+
+
+
+
