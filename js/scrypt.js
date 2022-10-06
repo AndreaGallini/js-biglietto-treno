bfebdadc98f3b9b816a5428d9bb2a9a122781971
AndreaGallini <andrea.gallini01@libero.it>
2022-10-06 16:10:21 +0200

    Add

--- js/scrypt.js
+++ js/scrypt.js
@@ -12,18 +12,20 @@ const over65 = 65;
 let costoKm = numeroKm * costoalKm;
 let costo = Math.ceil(costoKm)
 let costoIntero = costo.toFixed()
+let costoMultipli2 = costoIntero * 2;
+let costoMultipli3 = costoIntero * 3; 
 console.log(costoIntero)
 if(isNaN(numeroKm)|| isNaN(etaUtente)){
     alert('ricarica la pagina')
 }
-if(etaUtente < minorenni){
+if(etaUtente <= minorenni){
     let scontoMinorenni = (costoIntero * 20) / 100;
     let costoMinorenni = costoIntero - scontoMinorenni;
     console.log(costoMinorenni)
     document.getElementById('prezzoriservato').innerHTML = costoMinorenni + ' ' + '€'
 
     
-} else if( etaUtente > over65){
+} else if( etaUtente >= over65){
     let scontoOver65 = (costoIntero * 40) / 100;
     let costoOver65 = costoIntero - scontoOver65;
     console.log(costoOver65)
@@ -32,6 +34,7 @@ if(etaUtente < minorenni){
 }else if( (etaUtente > minorenni) || (etaUtente < over65)){
     document.getElementById('prezzoriservato').innerHTML = costoIntero
 }
+
 
 
 
